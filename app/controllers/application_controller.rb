7eec524a72af01e6523da4cec43c5c599aab9031
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-07-11 23:34:04 -0700

    commit tree f395f7b218e19b1d9b9c8946319a739287a5137f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.all.order(:bakery)
+    bakeries.to_json
+  end
+
+  
 
 end
